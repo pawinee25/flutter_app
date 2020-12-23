9933fed4c0bed25b95cb7daa4160301ee6df7460
patthum <patthum.2540@gmail.com>
2020-12-23 14:53:26 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,101 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String title = "Hora App";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "$title",
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: MyHomePage(
+        title: title,
       ),
-      home: MyHomePage(title: 'Flutter'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
   final String title;
+
+  MyHomePage({this.title});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Image.asset("assets/hora.png"),
         title: Text(widget.title),
+        backgroundColor: Colors.cyan,
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            _inputFormSection(),
+            _displayPredictionSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _inputFormSection() {
+    return Form(
+      child: Padding(
+        padding:
+            EdgeInsets.only(left: 10.0, top: 15.0, right: 10.0, bottom: 5.0),
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: "ชื่อ (name)",
+                labelStyle: TextStyle(color: Colors.redAccent, fontSize: 22.0),
+                hintText: "กรุณาป้อนชื่อ",
+                helperText: "* ข้อมูลที่จะต้องป้อน",
+                helperStyle: Theme.of(context)
+                    .textTheme
+                    .caption
+                    .copyWith(color: Colors.red),
+                suffixIcon: Icon(Icons.person, color: Colors.blue),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+              ),
+              maxLength: 30,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Container(
+              child: Center(
+                child: Row(
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text(
+                        "คำนวณ/ทำนาย",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: Colors.red,
+                      elevation: 50,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                      onPressed: () {},
+                    )
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
   }
+
+  _displayPredictionSection() => Text("Display Prediction Section.");
 }
